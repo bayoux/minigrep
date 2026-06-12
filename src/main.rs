c13874f23b8b6fccf2240b3f89276ee2d9f79db2
dayan <daniyar.sharshenbaev@energobank.kg>
2026-06-12 10:04:34 +0600

    feat: add output line number with new flag --ignore-case

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,43 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
-        eprintln!("Usage: minigrep <pattern> <file>");
+        eprintln!("Usage: minigrep <pattern> <file> [--ignore-case]");
         process::exit(1);
     }
 
     let pattern = &args[1];
     let file_path = &args[2];
 
+    let ignore_case = args.iter().any(|arg| arg == "--ignore-case");
+
     let contents = fs::read_to_string(file_path).unwrap_or_else(|err| {
         eprintln!("Error reading file: {err}");
         process::exit(1);
     });
 
-    let results = search(pattern, &contents);
-    
-    for line in results {
-        println!("{line}");
+    for (line_number, line) in search(pattern, &contents, ignore_case) {
+        println!("{line_number}: {line}");
     }
 }
 
-fn search<'a>(pattern: &str, contents: &'a str) -> Vec<&'a str> {
+fn search<'a>(pattern: &str, contents: &'a str, ignore_case: bool) -> Vec<(usize, &'a str)> {
     let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(pattern) {
-            results.push(line);
+    let pattern = if ignore_case {
+        pattern.to_lowercase()
+    } else {
+        pattern.to_string()
+    };
+
+    for (i, line) in contents.lines().enumerate() {
+        let line_to_check = if ignore_case {
+            line.to_lowercase()
+        } else {
+            line.to_string()
+        };
+
+        if line_to_check.contains(&pattern) {
+            results.push((i + 1, line));
         }
     }
 
